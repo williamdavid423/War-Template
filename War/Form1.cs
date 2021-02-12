6fc083698754f2f2de47061c18bc0f2b2bab1154
billy david <williamdavid423@hotmail.com>
2021-02-12 14:31:37 -0500

    reference for guessing game

--- War/Form1.cs
+++ War/Form1.cs
@@ -13,11 +13,15 @@ namespace War
     public partial class Form1 : Form
     {
         //random generator for picking new cards   
+        Random randGen = new Random();
 
         //variables to hold card values 
+        int playerCard;
+            int cpuCard;
 
         //variables to hold scores, initialized to 0
-
+        int playerScore = 0;
+        int cpuScore = 0;
 
         public Form1()
         {
@@ -31,24 +35,40 @@ namespace War
             cpuCardLabel.Image = null;
 
             //get random value between 1 and 10 for both player and cpu
-
-            
+            playerCard = randGen.Next(1,11);
+            cpuCard = randGen.Next(1, 11);
             //display card values to the labels
-
-
+            playerCardLabel.Text = $"{playerCard}";
+            cpuCardLabel.Text = $"{cpuCard}";
             // if player value is greater then cpu card
             //    add to player score 
             //    show new player score 
             //    show message saying player won
-              
+            if(playerCard > cpuCard)
+            {
+                playerScore = playerScore + 1;
+                playerScoreLabel.Text = $"{playerScore}";
+                outputLabel.Text = "player wins";
+            }
+           
+
             // if player value is less then cpu card
             //    add to cpu score
             //    show new cpu score
             //    show message saying cpu won
-              
+            if(cpuCard > playerCard)
+            {
+                cpuScore = cpuScore + 1;
+                cpuScoreLabel.Text = $"{cpuScore}";
+                outputLabel.Text = "cpu wins";
+            }
+
             // if player value is equal to cpu value 
             //    show message that this round is a tie.                  
-
+            if(playerCard == cpuCard)
+            {
+                outputLabel.Text = "it's a tie";
+            }
         }
     }
 }
